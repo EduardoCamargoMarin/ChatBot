9d00e011874e7bba37cdca39f230e70c46c65109
Eduardo Marin <108038506+EduardoCamargoMarin@users.noreply.github.com>
2023-05-03 17:45:06 -0300

    Add files via upload

--- Chatbot/Lara/index.js
+++ Chatbot/Lara/index.js
@@ -1,19 +1,35 @@
 //Safira alem das principais tarefas de responder questionamento simples, também indica assuntos pertinentes sobre o site
+
+//armazenamento das perguntas feitas ao chatbot
+const botMessage = [
+    'qual o seu nome',
+    'qual a sua função',
+    'qual a sua funcao',
+    'vagas de emprego',
+    'vagas',
+    'novidades',
+    'me conte as novidades',
+]
+
+
+
+
+
 function question() {
     let inputQuestion = document.getElementById('question').value
-
     let outputQuestion = document.getElementById('outputValue')
+
     let btnOption01 = document.getElementById('option01')
     let btnOption02 = document.getElementById('option02')
     let btnOption03 = document.getElementById('option03')
+
     let answer = document.getElementById('outputAnswer')
 
     outputQuestion.innerText = `${inputQuestion}?`
 
     if(inputQuestion === 'qual o seu nome') {
         outputQuestion.innerText = `${inputQuestion}?`
-        answer.innerText =  'Sou a Safira'
-        
+        answer.innerText =  'Sou a Lara'
         let btnAction = document.createElement('button')
         btnAction.innerHTML = 'Novidades'
 
@@ -23,10 +39,13 @@ function question() {
         })
         
         btnOption01.appendChild(btnAction) // faz aparecer o elemento criado no js
+
     }
     if(inputQuestion === 'qual a sua função' || inputQuestion === 'qual a sua funcao') {
+        
         outputQuestion.innerText = `${inputQuestion}?`
         answer.innerText =  'Sou uma ChatBot de teste para fins de auxiliar o usuário.' 
+
         let btnAction = document.createElement('button')
         btnAction.innerHTML = 'Veja as funcionalidades'
         
@@ -34,11 +53,11 @@ function question() {
     }
     if(inputQuestion === 'vagas de emprego' || inputQuestion === 'vagas') {
         outputQuestion.innerText = `Possuem ${inputQuestion}?`
-        answer.innerText =  'Atualmente estamos contratando para as seguintes vagas..... ' 
+        answer.innerText =  'Atualmente estamos contratando para as seguintes vagas..... '
         let btnAction = document.createElement('button')
         btnAction.innerHTML = 'Saiba mais'
         
-        btnOption01.appendChild(btnAction) 
+        btnOption02.appendChild(btnAction) 
     }
     if(inputQuestion === 'novidades' || inputQuestion === 'me conte as novidades') {
         outputQuestion.innerText = ` ${inputQuestion}?`
@@ -48,6 +67,10 @@ function question() {
         btnAction.innerHTML = 'Veja esse item aqui também'
         
         btnOption01.appendChild(btnAction)
+    }
+    if(inputQuestion === 'limpar' || inputQuestion ==='limpe') {
+        outputQuestion.innerText = ''
+        answer.innerText = ''
     }
 }
 
